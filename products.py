ac42d613f6a8d728dace9a350d0353a74c101ff0
AdamLin-73 <chiaweilin767312@gmail.com>
2020-08-19 02:24:43 +0800

    refactor

--- products.py
+++ products.py
@@ -1,44 +1,58 @@
 import os #operating system 
 
-products = []
-if os.path.isfile('products.csv'): #check folder if have products.csv file inside. 
-	print('yeah! 找到檔案了')
- 	with open('products.csv', 'r', encoding = 'utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding = 'utf-8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue #keep going
 			name, price = line.strip().split(',') #速寫
 			products.append([name, price])
-	print(products)
-
-else:
-	print('找不到檔案......')
-#讀取
-
+	return products	
 #		s = line.strip().split(',')
 #		name = s[0]
 #		price = s[1]
 #		print(s)
-while  True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	price = int(price)
-#	p = [] #三行寫法
-#	p.append(name)
-#	p.append(price)
-#	products.append(p)
-	
-#	p = [name, price] ＃快速寫法
-#	products.append(p)
-	products.append([name, price])
-print(products)
 
-for p in products:
-	print(p[0], '的價格是', p[1])
+def user_input(products):
+	while  True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		price = int(price)
+	#	p = [] #三行寫法
+	#	p.append(name)
+	#	p.append(price)
+	#	products.append(p)
+		
+	#	p = [name, price] ＃快速寫法
+	#	products.append(p)
+		products.append([name, price])
+	print(products)
+	return products
 
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品,價格\n')
+def print_products(products):
 	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+		print(p[0], '的價格是', p[1])
+
+def writ_file(filename, products):
+	with open(filename, 'w', encoding = 'utf-8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+def main ():
+	filename = 'products.csv'
+	if os.path.isfile(filename): #check folder if have products.csv file inside. 
+		print('yeah! 找到檔案了')	
+		products = read_file(filename)
+	else:
+		print('找不到檔案......')
+		
+	products = user_input(products)
+	print_products(products)
+	writ_file('products.csv', products)
+
+main()
